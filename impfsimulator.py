b4a1de66cfc4efb2ee1e025ba9ea69f8e70ac217
Darkestry <73218288+Darkestry@users.noreply.github.com>
2021-05-10 16:40:55 +0200

    Initialising # 1.1

--- impfsimulator.py
+++ impfsimulator.py
@@ -4,7 +4,7 @@ from glob import glob
 
 stock_files = sorted(glob("data/data_file_*.csv"))
 
-pd.concat((pd.read_csv(file).assign(filename=file)
+df = pd.concat((pd.read_csv(file).assign(filename=file)
            for file in stock_files), ignore_index=True)
 
-
+print(df.head())
