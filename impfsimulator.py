b76a2f18e4b0ec8ea4c5962fe45d596fdd3ff89f
philippti <philippt@dhbw-loerrach.de>
2021-05-10 18:11:37 +0200

    initilize queue

--- impfsimulator.py
+++ impfsimulator.py
@@ -1,6 +1,8 @@
+from numpy.core.numeric import NaN
 import pandas as pd
 import matplotlib
 from glob import glob
+from queue import PriorityQueue
 
 stock_files = sorted(glob("data/data_file_*.csv"))
 
@@ -9,4 +11,17 @@ vacFrame = pd.read_csv("data_file_2.csv")
 
 df = pd.concat([patientenFrame, vacFrame], axis=1, ignore_index=True)
 
-print(df.head(-1))
+patient_queue = PriorityQueue()
+first_vacc_queue = PriorityQueue()
+second_vacc_queue = PriorityQueue()
+
+for row in df:
+    patient = (df[1], df[2])
+    patient_queue.put((0, patient))
+
+for days in df[3].iteritems():
+    if pd.notnull(days[1]):
+    daily_capacity = df[4] + df[5] + df[6]
+    # print(daily_capacity)
+
+
