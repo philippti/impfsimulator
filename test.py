74d2a8c306513664987a334d24ffe8e50ac2c815
Darkestry <73218288+Darkestry@users.noreply.github.com>
2021-05-12 00:39:56 +0200

    Fertig für Data Visualisation Version... #6

--- test.py
+++ test.py
@@ -1,100 +1,101 @@
 import pandas as pd
 import matplotlib
 from glob import glob
-from queue import PriorityQueue
-from collections import OrderedDict
+import random
+from collections import Counter
+
 
 """PROCESS GIVEN DATA"""
 stock_files = sorted(glob("data/data_file_*.csv"))
 patFrame = pd.read_csv("data_file_1.csv")
 vacFrame = pd.read_csv("data_file_2.csv")
 
-daily_capacity = 0  # DAILY VACCINE CAPACITY
-
-df = pd.concat([patFrame, vacFrame], axis=1, ignore_index=True)  # CONCATENATE BOTH DATAFRAMES
+df = pd.concat([patFrame, vacFrame], axis=1, ignore_index=True)  # Concatenate both dataframes
 
 """CREATE QUEUES"""
-patient_queue = PriorityQueue()  # QUEUE FOR ALL PATIENTS
-
-vacA1_queue = PriorityQueue()
-vacB1_queue = PriorityQueue()  # QUEUES FOR 1ST VACCINATION
-vacC1_queue = PriorityQueue()
-
-vacA1_cache_queue = PriorityQueue()
-vacB1_cache_queue = PriorityQueue()  # QUEUES TO WAIT 4 WEEKS
-vacC1_cache_queue = PriorityQueue()
-
-vacA2_queue = PriorityQueue()
-vacB2_queue = PriorityQueue()  # QUEUES FOR 2ND VACCINATION
-vacC2_queue = PriorityQueue()
-
-immune_patients = []  # List of immune patients
+patient_queue = []  # List of all patients
 
 """SEGMENT PEOPLE INTO PRIORITIES"""
-patients = OrderedDict()
+patients = []
 for index, row in df[[0, 1, 2]].iterrows():
-    patients.update(
-        {index: {"Patient ID": index,  # THIS SHOULD PROBABLY BE A DICT COMP, (maybe df[0,1,2].to_dict('index'))
-                 "Patient Age": row[1],
-                 "Has Precondition": row[2]}})
-print(patients[0])
+    patients.append({"Patient ID": index,
+                     "Patient Age": row[1],
+                     "Has Precondition": row[2],
+                     "first_vaccination_date": None,
+                     "second_vaccination_date": None,
+                     "vaccine_provider": None})
 
 for patient in patients:
-    if patients[patient]["Patient Age"] >= 75 and patients[patient]["Has Precondition"] == 1:
-        patient_queue.put((0, patient))
-    elif patients[patient]["Patient Age"] >= 60 or patients[patient]["Has Precondition"] == 1:
-        patient_queue.put((1, patient))
+    if patient["Patient Age"] >= 75 and patient["Has Precondition"] == 1:
+        patient_queue.append((0, patient))
+    elif patient["Patient Age"] >= 60 or patient["Has Precondition"] == 1:
+        patient_queue.append((1, patient))
     else:
-        patient_queue.put((2, patient))
-
+        patient_queue.append((2, patient))
 """ASSIGN DAILY CAPACITY"""
-vac1_capacity = df[4].values  # VAC A CAPACITY PER DAY
-vac2_capacity = df[5].values  # VAC B CAPACITY PER DAY
-vac3_capacity = df[6].values  # VAC C CAPACITY PER DAY
+vac1_capacity = df[4].values  # Vac A Capacity per day
+vac2_capacity = df[5].values  # Vac B Capacity per day
+vac3_capacity = df[6].values  # Vac C Capacity per day
 
 daily_vac1_capacity = [int(x) for x in vac1_capacity if str(x) != "nan"]  # Data Cleansing
 daily_vac2_capacity = [int(x) for x in vac2_capacity if str(x) != "nan"]  # Data Cleansing
 daily_vac3_capacity = [int(x) for x in vac3_capacity if str(x) != "nan"]  # Data Cleansing
-#(daily_vac1_capacity)
 
-"""ASSIGN AVAILABLE VACCINES"""
-"""VAC A"""
-for days, vac_cap in enumerate(daily_vac1_capacity):
-    if days < 14:
-        for vac in range(vac_cap):
-            vacA1_cache_queue.put(patient_queue.get())
-    if days >= 42:
-        vacA2_queue.put(vacA1_cache_queue.get())
-        break
-    else:
-        for vac in range(vac_cap):
-            vacA1_queue.put(patient_queue.get())
-print("fertig mit daily vac 1")
-"""VAC B"""
-for days_2, vac_cap in enumerate(daily_vac2_capacity):
-    if days_2 < 14:
-        for vac in range(vac_cap):
-            vacB1_queue.put(patient_queue.get())
-    if days_2 >= 42:
-        vacB2_queue.put(vacB1_cache_queue.get())
-        print("test2")
-        break
-    else:
-        for vac in range(vac_cap):
-            vacB1_queue.put(patient_queue.get())
-            print("test")
-print("fertig mit daily vac 2")
-"""VAC C"""
-for days_3, vac_cap in enumerate(daily_vac3_capacity):
-    if days_3 < 14:
-        for vac in range(vac_cap):
-            vacC1_queue.put(patient_queue.get())
-    if days_3 >= 42:
-        vacC2_queue.put(vacC1_cache_queue.get())
-        break
-    else:
-        for vac in range(vac_cap):
-            vacC1_queue.put(patient_queue.get())
+total_capacity_dict = [{"A": a, "B": b, "C": c} for (a, b, c) in
+                       zip(daily_vac1_capacity, daily_vac2_capacity, daily_vac3_capacity)]
+print(total_capacity_dict)
 
-while not vacA2_queue.empty():
-    print(vacA2_queue.get())
+
+def prioritize_eligible(patients, day):
+    '''
+    Yields a random eligible patient, starting with the highest priority patients and going down.
+    Only hands back patients who are eligible for their first vaccine or who have waited long enough to be eligble for their second vaccine.
+    '''
+    patients = sorted(patients, key=lambda p: p[0])
+    for patient_priority, patient in patients:
+        if patient["first_vaccination_date"] is not None and patient["second_vaccination_date"] is not None:
+            continue
+        if patient["first_vaccination_date"] is not None and day - patient["first_vaccination_date"] < 28:
+            continue
+        yield patient
+
+
+
+def run_vaccinations(days, patient_queue):
+    total_vac_capacity = Counter({"A": 0, "B": 0, "C": 0})
+    for day in range(min(days, len(total_capacity_dict))):
+        vaccine_capacities = Counter(total_capacity_dict[day])
+        total_vac_capacity += vaccine_capacities
+        for patient in prioritize_eligible(patient_queue, day):
+            if not any(total_vac_capacity.values()):
+                continue  # Only update vaccination dates if there are vaccines remaining.
+
+            if not patient["first_vaccination_date"]:
+                # Choose a provider/vaccine for the patient.
+                provider = random.choice([k for (k, v) in total_vac_capacity.items() if
+                                          v > 0])  # Choose a random provider/vaccine if it has capacity.
+                patient["vaccine_provider"] = provider
+                # Administer the vaccine.
+                total_vac_capacity[provider] -= 1
+                patient["first_vaccination_date"] = day
+            elif not patient["second_vaccination_date"] and total_vac_capacity[patient["vaccine_provider"]] > 0:
+                # Administer the vaccine a second time.
+                total_vac_capacity[patient["vaccine_provider"]] -= 1
+                patient["second_vaccination_date"] = day
+
+    return patient_queue, total_vac_capacity  # A deep copy of whatever data we want, I'd recommend the day, the patients list, and the vaccine capacities list.
+
+
+def main(patient_queue):
+    report, capacity = run_vaccinations(305, patient_queue)
+    low_priority_patients = [(p["Patient ID"], p["first_vaccination_date"], p["second_vaccination_date"]) for pi, p in report if p["first_vaccination_date"] == 100]
+    print(capacity)
+    import pprint  # Can be removed if needed
+    pprint.pprint(low_priority_patients)
+
+    # Every day's relevant data is available in that report.
+    # You can also call list on run_vaccinations and get the reports for every day so you can run analyses on them
+    # Converting this to be more efficient using pandas/dataframes is an option, but only if Shara wants to :)
+
+
+main(patient_queue)
